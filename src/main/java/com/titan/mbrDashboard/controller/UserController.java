d3b3383f495bfca2749f463f80328b637b7293ed
ChinmayTitan <chinmay.mathakari@titan.co.in>
2024-12-06 17:08:41 +0530

    changes for controller

--- src/main/java/com/titan/mbrDashboard/controller/UserController.java
+++ src/main/java/com/titan/mbrDashboard/controller/UserController.java
@@ -3332,4 +3332,26 @@ public List<OutputForMontlyFilter> MonthlyTrendRegular(HttpServletRequest reques
 		return result;
 	}
 
+	
+	@RequestMapping(value = "growthOverPreviousYearMonthly", method = RequestMethod.POST)
+	public List<OutputGrowthOverPreviousMonth> growthOverPreviousYearMonthly(HttpServletRequest request, HttpServletResponse response,
+			RedirectAttributes redirect, Model model, @RequestBody MonthlyDataFilter filter) {
+		List<OutputGrowthOverPreviousMonth> result = new ArrayList<>();
+
+		HttpSession session = request.getSession();
+		String loginId = "";
+//			try {
+//				Map<String, Object> userMap = (Map) session.getAttribute("userMap");
+//				loginId = (String) userMap.get("login_id");
+//			} catch (Exception er) {
+//				er.printStackTrace();
+//				//return new StringBuilder("Session is timeout, <a href='login' >click here</a> to login");
+//			}
+
+		result = userService.growthOverPreviousYearMonthly(filter);
+		redirect.addFlashAttribute("MESSAGE", result);
+		model.addAttribute("MESSAGE", result);
+		return result;
+	}
+	
 }
